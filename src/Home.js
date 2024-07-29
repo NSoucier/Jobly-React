7e7f08596562b613e659647265bbd592f81be0a4
Nathan Soucier <soucienp@gmail.com>
2024-07-29 12:50:24 -0400

    added home message

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ function Home({ user }) {
     return (
         <>
             <h2 className='welcome'>{ user ? `Welcome back to the hunt, ${user}!` : 'Welcome to the hunt!'}</h2>
+            <h5 style={{color: 'white'}}>Please login or sign up to proceed.</h5>
             {/* <h5 className='welcome'>Please login or sign up to see all jobs and companies.</h5> */}
         </>
 
